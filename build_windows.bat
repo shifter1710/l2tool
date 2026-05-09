f0c956ffe110b0dfd08571012aa496ce762e85da
shifter1710 <shifter1710@users.noreply.github.com>
2026-05-09 08:59:27 +0000

    Fix Windows timezone data for build

--- build_windows.bat
+++ build_windows.bat
@@ -4,7 +4,8 @@ setlocal
 cd /d "%~dp0"
 
 py -m pip install --upgrade pyinstaller
-py -m PyInstaller --noconfirm --onefile --windowed --name l2tool gui.py
+py -m pip install -r requirements.txt
+py -m PyInstaller --noconfirm --onefile --windowed --name l2tool --collect-all tzdata gui.py
 
 echo.
 echo Built: dist\l2tool.exe
